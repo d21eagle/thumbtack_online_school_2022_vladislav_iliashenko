639321785697d5e05f32637a64881c9169e343c1
Pavel Dvorkin <pdvorkin@lineate.com>
2022-11-06 12:12:46 +0600

    code review, part 2

--- src/test/java/net/thumbtack/school/hiring/service/TestEmployeeService.java
+++ src/test/java/net/thumbtack/school/hiring/service/TestEmployeeService.java
@@ -24,8 +24,12 @@ public class TestEmployeeService {
                 "754376579"
         );
 
+        // REVU нет, не имеете тут права доступа к сервису. Только к серверу. Его метод и вызывайте
         EmployeeService employeeService = new EmployeeService();
         ServerResponse actualResponse = employeeService.registerEmployee(GSON.toJson(requestJson));
+        // REVU не надо создавать EmptyResponse и проверять
+        // достаточно проверить код в ServerResponse
+        // Если он 200 - ввызов метода был успешен
         EmptyResponse emptyResponse = new EmptyResponse();
         ServerResponse expectedResponse = new ServerResponse(SUCCESS_CODE, GSON.toJson(emptyResponse));
         assertTrue(expectedResponse.equals(actualResponse));
@@ -35,6 +39,13 @@ public class TestEmployeeService {
     public void TestRegisterEmployee_Failed() {
         final int ERROR_CODE = 400;
         final Gson GSON = new Gson();
+        // REVU у Вас тут нет права доступа к внутренностям сервера
+        // а именно
+        // к сервисам
+        // к модели
+        // к исключениям
+        // к БД
+        // да и не нужно это
         ServerException e = new ServerException(ServerErrorCode.SHORT_PASSWORD);
 
         RegisterEmployeeDtoRequest requestJson = new RegisterEmployeeDtoRequest(
@@ -47,7 +58,15 @@ public class TestEmployeeService {
         );
 
         EmployeeService employeeService = new EmployeeService();
+        // REVU нет, не имеете тут права доступа к сервису. Только к серверу. Его метод и вызывайте
         ServerResponse actualResponse = employeeService.registerEmployee(GSON.toJson(requestJson));
+        // REVU а тут надо проверить код из actualResponse, он должен быть 400
+        // а потом получить ErrorResponse из  actualResponse.data и проверить текст
+        // а кстати, почему этот тест должен упасть ?
+        // потому что уже такое было ?
+        // тесты независимы, порядок их запуска неопределен
+        // и после (или перед) запуском теста надо чистить БД
+        // @BeforeEach или @AfterEach
         ErrorResponse errorResponse = new ErrorResponse(e);
         ServerResponse expectedResponse = new ServerResponse(ERROR_CODE, GSON.toJson(errorResponse));
         assertTrue(expectedResponse.equals(actualResponse));
