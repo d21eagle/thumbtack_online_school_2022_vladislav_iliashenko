34800efd59c426c6b75249cbbc24e5c0d21b5bd6
Vlad Ilyashenko <vlad180317@gmail.com>
2022-11-27 19:44:28 +0000

    +правки

--- src/main/java/net/thumbtack/school/hiring/dto/request/LoginEmployerDtoRequest.java
+++
@@ -1,9 +0,0 @@
-package net.thumbtack.school.hiring.dto.request;
-import lombok.*;
-
-@Data
-@AllArgsConstructor
-public class LoginEmployerDtoRequest {
-    private String login;
-    private String password;
-}
